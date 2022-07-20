c604ce4019661c2e99ed0af917ab589f823e301c
MING-TA YANG <startover205@gmail.com>
2022-07-20 11:05:13 +0800

    Initial Commit

---
+++ Shared/HelloBluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloBluetoothApp.swift
+//  Shared
+//
+//  Created by Ming-Ta Yang on 2022/7/20.
+//
+
+import SwiftUI
+
+@main
+struct HelloBluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
